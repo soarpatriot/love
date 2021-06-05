6d30f9ee4d12dbd57fe794729685d43762cc4ceb
Soar <>
2021-06-05 21:52:12 +0800

    doing the first page

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -18,51 +18,56 @@ Page({
     })
   },
 
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        // wx.navigateTo({
-        //   url: '../userConsole/userConsole',
-        // })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
+  onLoad: async function (options) {
+    app.globalData.openid = app.globalData.openid ||
+      (await wx.cloud.callFunction({name:'login'})).result.openid
+    console.log(app.globalData.openid)
   },
+  // onLoad: function() {
+  //   if (!wx.cloud) {
+  //     wx.redirectTo({
+  //       url: '../chooseLib/chooseLib',
+  //     })
+  //     return
+  //   }
+
+  //   // 调用云函数
+  //   wx.cloud.callFunction({
+  //     name: 'login',
+  //     data: {},
+  //     success: res => {
+  //       console.log('[云函数] [login] user openid: ', res.result.openid)
+  //       app.globalData.openid = res.result.openid
+  //       // wx.navigateTo({
+  //       //   url: '../userConsole/userConsole',
+  //       // })
+  //     },
+  //     fail: err => {
+  //       console.error('[云函数] [login] 调用失败', err)
+  //       wx.navigateTo({
+  //         url: '../deployFunctions/deployFunctions',
+  //       })
+  //     }
+  //   })
+
+
+  //   // 获取用户信息
+  //   wx.getSetting({
+  //     success: res => {
+  //       if (res.authSetting['scope.userInfo']) {
+  //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+  //         wx.getUserInfo({
+  //           success: res => {
+  //             this.setData({
+  //               avatarUrl: res.userInfo.avatarUrl,
+  //               userInfo: res.userInfo
+  //             })
+  //           }
+  //         })
+  //       }
+  //     }
+  //   })
+  // },
 
   onGetUserInfo: function(e) {
     if (!this.data.logged && e.detail.userInfo) {
