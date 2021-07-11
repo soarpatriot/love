fcd807f43378f186f5481a00acb8cb7dff066d46
Soar <>
2021-07-11 18:38:15 +0800

    change color

--- miniprogram/libs/utils.js
+++ miniprogram/libs/utils.js
@@ -1,12 +1,12 @@
 
 
 //module.exports.categories = ['emotion', 'potential', 'belief','life','love' ]
-
+//  #Ed0f0f
 module.exports.categories = [
   {key: 'emotion', color: '#51cb5d'},
   {key: 'potential', color: '#170406'},
   {key: 'belief', color: '#2587be'},
   {key: 'life', color: '#6c25be'},
-  {key: 'love', color: '#Ed0f0f'}
+  {key: 'love', color: '#FF6670'}
 ]
 
